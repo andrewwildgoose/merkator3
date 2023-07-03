880837f2b52ad8c166c965e2cbaceaded427aa12
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2023-07-03 19:12:30 +0100

    initial getUserTest passed in UserServiceTests.

--- src/test/java/com/merkator3/merkator3api/unitTests/serviceTests/UserServiceTests.java
+++ src/test/java/com/merkator3/merkator3api/unitTests/serviceTests/UserServiceTests.java
@@ -6,9 +6,11 @@ import com.merkator3.merkator3api.models.User;
 import com.merkator3.merkator3api.repositories.UserRepository;
 import com.merkator3.merkator3api.services.UserService;
 import com.merkator3.merkator3api.services.UserServiceImpl;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.AfterTestExecution;
 
 @SpringBootTest
 public class UserServiceTests {
@@ -22,9 +24,27 @@ public class UserServiceTests {
         userService = new UserServiceImpl(userRepository);
     }
 
+    @AfterEach
+    void deleteTestUsers() {
+        if (userRepository.findByUserName("merkatorUser1")!=null){
+            userRepository.delete(userRepository.findByUserName("merkatorUser1"));
+        }
+        if (userRepository.findByUserName("merkatorUser2")!=null){
+            userRepository.delete(userRepository.findByUserName("merkatorUser2"));
+        }
+    }
+
     @Test
     void createUserTest() {
         userService.createUser("merkatorUser1");
         Assertions.assertEquals(userRepository.findByUserName("merkatorUser1").getUserName(), "merkatorUser1");
+    }
+
+    @Test
+    void getUserTest() {
+        userService.createUser("merkatorUser2");
+        ObjectId testUserId = userRepository.findByUserName("merkatorUser2").getId();
+        User testUser = userService.getUser(testUserId);
+        Assertions.assertEquals(testUser.getId(),  testUserId);
     }
 }
